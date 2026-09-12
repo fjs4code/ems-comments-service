ee116e2cb1d1dc66ffc792dd17d49b751df90636
frank <frank.apk@outlook.com>
2026-09-11 22:25:54 -0400

    init commit

---
+++ src/main/java/com/fjs/algacomments/comments_service/CommentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fjs.algacomments.comments_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fjs/algacomments/comments_service/CommentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjs.algacomments.comments_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
